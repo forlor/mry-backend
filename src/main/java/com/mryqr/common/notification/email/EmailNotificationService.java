f6b6fe04da2fa0378307a6b74e9f39b61b9cc079
Teng Yun <davenkin@126.com>
2024-02-20 21:35:44 +0800

    added email notification

--- src/main/java/com/mryqr/common/notification/email/EmailNotificationService.java
+++ src/main/java/com/mryqr/common/notification/email/EmailNotificationService.java
@@ -1,24 +1,17 @@
 package com.mryqr.common.notification.email;
 
 import com.mryqr.common.notification.NotificationService;
-import com.mryqr.common.notification.wx.Response;
-import com.mryqr.common.notification.wx.ValueItem;
-import com.mryqr.common.notification.wx.WxTemplateMessage;
-import com.mryqr.common.wx.accesstoken.WxAccessTokenService;
 import com.mryqr.core.app.domain.App;
 import com.mryqr.core.app.domain.page.Page;
 import com.mryqr.core.app.domain.page.setting.notification.NotificationRole;
 import com.mryqr.core.assignment.domain.Assignment;
 import com.mryqr.core.common.properties.PropertyService;
-import com.mryqr.core.common.properties.WxProperties;
 import com.mryqr.core.group.domain.Group;
 import com.mryqr.core.group.domain.GroupRepository;
-import com.mryqr.core.member.domain.Member;
 import com.mryqr.core.member.domain.MemberRepository;
 import com.mryqr.core.qr.domain.QrRepository;
 import com.mryqr.core.submission.domain.Submission;
 import com.mryqr.core.submission.domain.SubmissionApproval;
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,15 +19,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import static com.mryqr.common.notification.wx.WxTemplateMessage.valueItemOf;
 import static com.mryqr.core.app.domain.page.setting.notification.NotificationRole.*;
 import static com.mryqr.core.common.utils.MryConstants.MRY_DATE_TIME_FORMATTER;
 import static com.mryqr.management.crm.MryTenantManageApp.MRY_TENANT_MANAGE_APP_ID;
@@ -49,14 +38,10 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 @Profile("prod")
 @RequiredArgsConstructor
 public class EmailNotificationService implements NotificationService {
-    private final WxProperties wxProperties;
     private final MemberRepository memberRepository;
     private final GroupRepository groupRepository;
     private final QrRepository qrRepository;
-    private final RestTemplate restTemplate;
-    private final WxAccessTokenService wxAccessTokenService;
     private final PropertyService propertyService;
-
     private final JavaMailSender mailSender;
 
     @Override
@@ -71,7 +56,9 @@ public class EmailNotificationService implements NotificationService {
         String qrName = qrRepository.qrNameOf(submission.getQrId());
         String submittedBy = isNotBlank(submission.getCreatedBy()) ? memberRepository.cachedMemberNameOf(submission.getCreatedBy()) : "匿名";
         String createdAt = MRY_DATE_TIME_FORMATTER.format(submission.getCreatedAt());
-        String subject = app.getId().equals(MRY_TENANT_MANAGE_APP_ID) && page.getId().equals("p_3nbM6aYj9y4FuaFTYrLADG") ? "码如云有新租户注册了！" : "请关注表单提交。";
+        String subject = app.getId().equals(MRY_TENANT_MANAGE_APP_ID) && page.getId().equals("p_3nbM6aYj9y4FuaFTYrLADG") ?
+                "码如云有新租户注册了！" :
+                "请关注表单提交（" + qrName + "）";
 
         String content = "<div style=\"margin-bottom:12px;\">您有新的表单提交需关注，详情如下：</div>\n" +
                          "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
@@ -109,7 +96,7 @@ public class EmailNotificationService implements NotificationService {
         String qrName = qrRepository.qrNameOf(submission.getQrId());
         String updatedBy = isNotBlank(submission.getUpdatedBy()) ? memberRepository.cachedMemberNameOf(submission.getUpdatedBy()) : "匿名";
         String updatedAt = MRY_DATE_TIME_FORMATTER.format(submission.getUpdatedAt());
-        String subject = "请关注表单更新。";
+        String subject = "请关注表单更新（" + qrName + "）";
 
         String content = "<div style=\"margin-bottom:12px;\">您有新的表单更新需关注，详情如下：</div>\n" +
                          "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
@@ -163,133 +150,104 @@ public class EmailNotificationService implements NotificationService {
         }
 
         memberRepository.cachedByIdOptional(submission.getCreatedBy()).stream()
-                .filter(member -> isNotBlank(member.getMobileWxOpenId()))
+                .filter(member -> isNotBlank(member.getEmail()))
                 .findFirst().ifPresent(member -> {
-                    WxTemplateMessage message = createNotifyOnApprovalMessage(submission, page, approval, member, app.getName());
-                    sendTemplateMessage(message);
+                    String url = submissionUrlOf(submission.getId(), page.getId(), submission.getPlateId());
+                    String qrName = qrRepository.qrNameOf(submission.getQrId());
+                    String approvedBy = isNotBlank(approval.getApprovedBy()) ? memberRepository.cachedMemberNameOf(approval.getApprovedBy()) : "匿名";
+                    Group group = groupRepository.cachedById(submission.getGroupId());
+                    String subject = "审批完成通知（" + qrName + "）";
+                    String approveResult = approval.isPassed() ? page.approvalPassText() : page.approvalNotPassText();
+                    String content = "<div style=\"margin-bottom:12px;\">您提交的表单已经完成审批，详情如下：</div>\n" +
+                                     "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                                     "  <span style=\"color:#909399;padding-right:10px;\">" + app.instanceDesignation() + "名称：</span>" + qrName + "\n" +
+                                     "</div>\n" +
+                                     "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                                     "  <span style=\"color:#909399;padding-right:10px;\">页面名称：</span>" + page.pageName() + "\n" +
+                                     "</div>\n" +
+                                     "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                                     "  <span style=\"color:#909399;padding-right:10px;\">所在" + app.groupDesignation() + "：</span>" + group.getName() + "\n" +
+                                     "</div>\n" +
+                                     "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                                     "  <span style=\"color:#909399;padding-right:10px;\">所在应用：</span>" + app.getName() + "\n" +
+                                     "</div>\n" +
+                                     "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                                     "  <span style=\"color:#909399;padding-right:10px;\">审批人：</span>" + approvedBy + "\n" +
+                                     "</div>\n" +
+                                     "<div style=\"padding-left:16px;\">\n" +
+                                     "  <span style=\"color:#909399;padding-right:10px;\">审批结果：</span>" + approveResult + "\n" +
+                                     "</div>\n" +
+                                     "<div style=\"margin-top:12px;\">如需查看表单详情，请点击<a href=\"" + url + "\" target=\"_blank\">此链接</a>。</div>\n";
+                    sendMail(member.getEmail(), subject, content);
                 });
     }
 
-    private WxTemplateMessage createNotifyOnApprovalMessage(Submission submission,
-                                                            Page page,
-                                                            SubmissionApproval approval,
-                                                            Member submitter,
-                                                            String appName) {
-        String url = submissionUrlOf(submission.getId(), page.getId(), submission.getPlateId());
-        String qrName = qrRepository.qrNameOf(submission.getQrId());
-        String approvedBy = isNotBlank(approval.getApprovedBy()) ? memberRepository.cachedMemberNameOf(approval.getApprovedBy()) : null;
-        Group group = groupRepository.cachedById(submission.getGroupId());
-
-        return WxTemplateMessage.builder()
-                .touser(submitter.getMobileWxOpenId())
-                .template_id(wxProperties.getSubmissionApprovedTemplateId())
-                .url(url)
-                .data(Map.of("first", valueItemOf(qrName),
-                        "thing2", valueItemOf(page.getSetting().getPageName()),
-                        "thing8", valueItemOf(appName),
-                        "thing9", valueItemOf(group.getName()),
-                        "phrase10", approveResult(page, approval),
-                        "thing6", valueItemOf(approvedBy),
-                        "remark", valueItemOf("点击可查看详情")
-                ))
-                .build();
-    }
-
-    private ValueItem approveResult(Page page, SubmissionApproval approval) {
-        return approval.isPassed() ?
-                valueItemOf(page.approvalPassText(), "#10B01B") :
-                valueItemOf(page.approvalNotPassText(), "#EA0000");
-    }
 
     @Override
     public void notifyOperatorsAssignmentNearExpire(Assignment assignment, App app) {
         List<String> operators = assignment.getOperators();
-        Set<String> operatorWxOpenIds = copyOf(memberRepository.cachedMobileWxOpenIdsOf(assignment.getTenantId(), operators).values());
-        if (isEmpty(operatorWxOpenIds)) {
+        Set<String> operatorEmails = copyOf(memberRepository.cachedEmailsOf(assignment.getTenantId(), operators).values());
+        if (isEmpty(operatorEmails)) {
             return;
         }
 
         String url = propertyService.clientBaseUrl() + "/operations/" + assignment.getAppId() + "/my-assignments/" + assignment.getId() + "/qrs";
-
-        operatorWxOpenIds.forEach(openId -> {
-            WxTemplateMessage message = createNotifyOnAssignmentNearExpireMessage(openId, url, assignment, app);
-            sendTemplateMessage(message);
+        String startTime = MRY_DATE_TIME_FORMATTER.format(assignment.getStartAt());
+        String endTime = MRY_DATE_TIME_FORMATTER.format(assignment.getExpireAt());
+        operatorEmails.forEach(emailTo -> {
+            String content = "<div style=\"margin-bottom:12px;\">以下任务即将过期，请关注：</div>\n" +
+                             "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">任务名称：</span>" + assignment.getName() + "\n" +
+                             "</div>\n" +
+                             "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">所在应用：</span>" + app.getName() + "\n" +
+                             "</div>\n" +
+                             "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">开始时间：</span>" + startTime + "\n" +
+                             "</div>\n" +
+                             "<div style=\"padding-left:16px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">结束时间：</span>" + endTime + "\n" +
+                             "</div>\n" +
+                             "<div style=\"margin-top:12px;\">如需查看任务详情，请点击<a href=\"" + url + "\" target=\"_blank\">此链接</a>。</div>\n";
+            String subject = "任务即将过期提醒（" + assignment.getName() + "）";
+            sendMail(emailTo, subject, content);
         });
-    }
-
-    private WxTemplateMessage createNotifyOnAssignmentNearExpireMessage(String openId,
-                                                                        String url,
-                                                                        Assignment assignment,
-                                                                        App app) {
-        return WxTemplateMessage.builder()
-                .touser(openId)
-                .template_id(wxProperties.getAssignmentNearExpireTemplateId())
-                .url(url)
-                .data(Map.of("first", valueItemOf("您有任务即将到期"),
-                        "thing2", valueItemOf(assignment.getName()),
-                        "time5", valueItemOf(MRY_DATE_TIME_FORMATTER.format(assignment.getStartAt())),
-                        "time3", valueItemOf(MRY_DATE_TIME_FORMATTER.format(assignment.getExpireAt())),
-                        "thing6", valueItemOf(app.getName()),
-                        "remark", valueItemOf("点击可查看任务详情")
-                ))
-                .build();
     }
 
     @Override
     public void notifyOperatorsOnAssignmentCreated(Assignment assignment, App app) {
         List<String> operators = assignment.getOperators();
-        Set<String> operatorWxOpenIds = copyOf(memberRepository.cachedMobileWxOpenIdsOf(assignment.getTenantId(), operators).values());
-        if (isEmpty(operatorWxOpenIds)) {
+        Set<String> operatorEmails = copyOf(memberRepository.cachedEmailsOf(assignment.getTenantId(), operators).values());
+        if (isEmpty(operatorEmails)) {
             return;
         }
 
         String url = propertyService.clientBaseUrl() + "/operations/" + assignment.getAppId() + "/my-assignments/" + assignment.getId() + "/qrs";
-
-        operatorWxOpenIds.forEach(openId -> {
-            WxTemplateMessage message = createNotifyOnAssignmentCreatedMessage(openId, url, assignment, app);
-            sendTemplateMessage(message);
+        String startTime = MRY_DATE_TIME_FORMATTER.format(assignment.getStartAt());
+        String endTime = MRY_DATE_TIME_FORMATTER.format(assignment.getExpireAt());
+        operatorEmails.forEach(emailTo -> {
+            String content = "<div style=\"margin-bottom:12px;\">您有新任务需要完成，请关注：</div>\n" +
+                             "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">任务名称：</span>" + assignment.getName() + "\n" +
+                             "</div>\n" +
+                             "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">所在应用：</span>" + app.getName() + "\n" +
+                             "</div>\n" +
+                             "<div style=\"padding-left:16px;margin-bottom:5px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">开始时间：</span>" + startTime + "\n" +
+                             "</div>\n" +
+                             "<div style=\"padding-left:16px;\">\n" +
+                             "  <span style=\"color:#909399;padding-right:10px;\">结束时间：</span>" + endTime + "\n" +
+                             "</div>\n" +
+                             "<div style=\"margin-top:12px;\">如需查看任务详情，请点击<a href=\"" + url + "\" target=\"_blank\">此链接</a>。</div>\n";
+            String subject = "您有新任务需要完成（" + assignment.getName() + "）";
+            sendMail(emailTo, subject, content);
         });
-    }
-
-    private WxTemplateMessage createNotifyOnAssignmentCreatedMessage(String openId,
-                                                                     String url,
-                                                                     Assignment assignment,
-                                                                     App app) {
-        return WxTemplateMessage.builder()
-                .touser(openId)
-                .template_id(wxProperties.getAssignmentNearExpireTemplateId())
-                .url(url)
-                .data(Map.of("first", valueItemOf("您有新任务需要完成"),
-                        "thing2", valueItemOf(assignment.getName()),
-                        "time5", valueItemOf(MRY_DATE_TIME_FORMATTER.format(assignment.getStartAt())),
-                        "time3", valueItemOf(MRY_DATE_TIME_FORMATTER.format(assignment.getExpireAt())),
-                        "thing6", valueItemOf(app.getName()),
-                        "remark", valueItemOf("点击可查看任务详情")
-                ))
-                .build();
     }
 
     private String submissionUrlOf(String submissionId, String pageId, String plateId) {
         return propertyService.clientBaseUrl() + "/r/" + plateId + "/pages/" + pageId + "/" + submissionId;
     }
-
-    private void sendTemplateMessage(WxTemplateMessage message) {
-        if (!wxProperties.isMobileWxEnabled()) {
-            return;
-        }
-
-        try {
-            String accessToken = wxAccessTokenService.getAccessToken();
-            String url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + accessToken;
-            Response response = restTemplate.postForObject(url, message, Response.class);
-            if (response == null || response.getErrcode() != 0) {
-                log.error("Failed to send wx template message[{}] with error[{}].", message, response);
-            }
-        } catch (Throwable t) {
-            log.error("Failed to send wx template message[{}].", message, t);
-        }
-    }
-
 
     private void sendMail(String mailTo, String subject, String content) {
         try {
