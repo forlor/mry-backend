c24c444b67921051994fb508ee078a050f187d81
Yun Yun4 Teng <tengyun4@lenovo.com>
2024-11-26 10:11:33 +0800

    update

--- src/main/java/com/mryqr/common/event/consume/infrastructure/MongoConsumingDomainEventDao.java
+++ src/main/java/com/mryqr/common/event/consume/infrastructure/MongoConsumingDomainEventDao.java
@@ -1,5 +1,8 @@
 package com.mryqr.common.event.consume.infrastructure;
 
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
 import com.mongodb.client.result.UpdateResult;
 import com.mryqr.common.event.consume.ConsumingDomainEvent;
 import com.mryqr.common.event.consume.ConsumingDomainEventDao;
@@ -10,28 +13,28 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class MongoConsumingDomainEventDao<T> implements ConsumingDomainEventDao<T> {
-    private final MongoTemplate mongoTemplate;
+  private final MongoTemplate mongoTemplate;
 
-    @Override
-    public boolean recordAsConsumed(ConsumingDomainEvent<T> consumingDomainEvent, String handlerName) {
-        Query query = query(where(ConsumingDomainEvent.Fields.eventId).is(consumingDomainEvent.getEventId()).and(ConsumingDomainEvent.Fields.handlerName).is(handlerName));
+  @Override
+  public boolean recordAsConsumed(ConsumingDomainEvent<T> consumingDomainEvent, String handlerName) {
+    Query query = query(where(ConsumingDomainEvent.Fields.eventId)
+        .is(consumingDomainEvent.getEventId())
+        .and(ConsumingDomainEvent.Fields.handlerName)
+        .is(handlerName));
 
-        Update update = new Update()
-                .setOnInsert(ConsumingDomainEvent.Fields.type, consumingDomainEvent.getType())
-                .setOnInsert(ConsumingDomainEvent.Fields.consumedAt, consumingDomainEvent.getConsumedAt());
+    Update update = new Update()
+        .setOnInsert(ConsumingDomainEvent.Fields.type, consumingDomainEvent.getType())
+        .setOnInsert(ConsumingDomainEvent.Fields.consumedAt, consumingDomainEvent.getConsumedAt());
 
-        UpdateResult result = this.mongoTemplate.update(ConsumingDomainEvent.class)
-                .matching(query)
-                .apply(update)
-                .upsert();
+    UpdateResult result = this.mongoTemplate.update(ConsumingDomainEvent.class)
+        .matching(query)
+        .apply(update)
+        .upsert();
 
-        return result.getMatchedCount() == 0;
-    }
+    return result.getMatchedCount() == 0;
+  }
 }
